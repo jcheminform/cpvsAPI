610c9472821b83fcf4941db7e4a137cbbe06571f
laeeq80 <laeeq@kth.se>
2019-05-27 17:28:21 +0200

    Adding resources

---
+++ resources.sh
@@ -0,0 +1,4 @@
+export VINA_CONF=/home/laeeq/Desktop/Backup/Project5/ReceptorsAndInhibitors/$1/conf.txt 
+export RECEPTOR_NAME=$1
+export RECEPTOR_PDBCODE=$1
+export RESOURCES_HOME=/home/laeeq/Desktop/Backup/Project5/ReceptorsAndInhibitors/$1/
